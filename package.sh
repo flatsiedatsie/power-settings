f4f7ee7981e8cee096b72d3ad6c42b55a5b2fa5b
flatsiedatsie <unfold@gmail.com>
2020-02-18 00:11:14 +0100

    fix for packaging issue
    maming sure .pyc files are deleted

--- package.sh
+++ package.sh
@@ -2,10 +2,16 @@
 
 version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
-rm -rf SHA256SUMS package
+rm -rf *.tgz SHA256SUMS package
+rm -rf ._*
+
 mkdir package
 cp *.py manifest.json LICENSE README.md package/
 cp -r pkg css images js views package/
+
+find package -type f -name '*.pyc' -delete
+find package -type d -empty -delete
+
 cd package
 find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
 cd ..
